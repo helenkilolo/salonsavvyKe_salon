359c8be63aa67e47e55e2b1f00f32961b0e841da
helenkilolo <helenkilolo@gmail.com>
2024-05-12 09:27:14 +0300

    hele

--- salonsavvyke/settings.py
+++ salonsavvyke/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=ddb5tt!q*d!7g%2__41c_e&i920l$r$knu5lm0axdddu*%w1g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
